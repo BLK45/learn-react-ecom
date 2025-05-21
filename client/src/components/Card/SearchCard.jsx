89ee9f1f7687f1f077ad39fdb764c16ca28be001
BLK45 <kullaya.9102@gmail.com>
2025-05-21 21:29:17 +0700

    Font-end-Ep15

--- client/src/components/Card/SearchCard.jsx
+++ client/src/components/Card/SearchCard.jsx
@@ -5,21 +5,53 @@ const SearchCard = () => {
 
   const getProduct = useEcomStore((state)=>state.getProduct)
   const products = useEcomStore((state)=>state.products)
+  const getCategory = useEcomStore((state)=>state.getCategory)
+  const categories = useEcomStore((state)=>state.categories)
   const actionSearchFilters = useEcomStore((state)=>state.actionSearchFilters)
-
+  
   const [text, setText] = useState('')
-  console.log(text)
+  const [categorySelected, setCategorySelected] = useState([])
+
+    useEffect(()=>{
+        getCategory()
+    }, [])
+
+    // step 1 Search by Text
+  
+//   console.log(text)
   useEffect(()=>{
     const delay = setTimeout(()=>{
-        actionSearchFilters({ query:text })
-        if(!text){
+        
+        if(text){
+           actionSearchFilters({ query:text }) 
+        }else{
             getProduct()
         }
     }, 300)
     return ()=> clearTimeout(delay)
   }, [text])
-    // step 1 Search by Text
+    
     // step 2 Search by Category
+    const handleCheck = (e) => {
+        // console.log(e.target.value)
+        const inCheck = e.target.value                  // ค่าเราติ๊ก
+        const inState = [...categorySelected]           // [] arr ว่าง
+        const findCheck = inState.indexOf(inCheck)      // ถ้าไม่เจอจะ return -1
+
+        if(findCheck === -1){
+            inState.push(inCheck)
+        }else{
+            inState.splice(findCheck, 1)
+        }
+        setCategorySelected(inState)
+        
+        if(inState>0){
+            actionSearchFilters({ category: inState})
+        }else{
+            getProduct()
+        }
+    }
+    console.log(categorySelected)
     // step 3 Search by Price
 
 
@@ -34,6 +66,24 @@ const SearchCard = () => {
         placeholder='ค้นหาสินค้า......'
         className='border rounded-md w-full mb-4 px-2'
         />
+        <hr />
+        <div>
+            <h1 >หมวดหมูสินค้า</h1>
+            <div>
+                {
+                    categories.map((item, index)=>
+                        <div className='flex gap-2'>
+                        <input
+                        onChange={handleCheck} 
+                        value={item.id}
+                        type='checkbox' 
+                        />
+                        <label>{item.name}</label>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     </div>
   )
 }
